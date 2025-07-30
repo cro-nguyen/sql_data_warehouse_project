7a3bf154b7e46dd1502c5dcedc4759a62edceaaa
hungnguyen <hoanghungmt@gmail.com>
2025-07-29 22:48:59 -0700

    Update 1.gold_create_view_table.sql

--- scripts/gold/1.gold_create_view_table.sql
+++ scripts/gold/1.gold_create_view_table.sql
@@ -1,3 +1,17 @@
+-- =====================================================================================
+-- Gold Layer Views - Data Warehouse Star Schema
+-- =====================================================================================
+-- Description: Creates dimensional views for the gold layer in a medallion architecture
+-- Author: Hung Nguyen
+-- Created: 2025-07-29
+-- Version: 1.0
+-- =====================================================================================
+
+-- Customer dimension view - combines CRM and ERP customer data
+IF OBJECT_ID('gold.dim_customers') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
 CREATE VIEW gold.dim_customers AS
 SELECT
        ROW_NUMBER() OVER ( ORDER BY cst_id) AS customer_key,
@@ -19,6 +33,11 @@ FROM silver.crm_cust_info ci
          LEFT JOIN silver.erp_loc_a101 la
                    ON ci.cst_key = la.cid;
 
+-- Product dimension view - includes active products with category hierarchy
+IF OBJECT_ID('gold.dim_products') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+
 CREATE VIEW gold.dim_products AS
 SELECT
         ROW_NUMBER() over (ORDER BY pn.prd_id, pn.prd_key) AS product_key,
@@ -36,3 +55,25 @@ FROM SILVER.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL;
+
+-- Sales fact view - central transaction table linking customers and products
+IF OBJECT_ID('gold.fact_sales') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sls_ord_num AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    sls_order_dt AS order_date,
+    sls_ship_dt AS ship_date,
+    sls_due_dt AS due_date,
+    sls_sales AS sales_amount,
+    sls_quantity AS quantity,
+    sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
